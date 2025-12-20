b1f829cc28463d41f9c91fb83b0a2f775f56aa44
swaroopstack <swarooppokemon@gmail.com>
2025-12-20 12:52:35 +0530

    intro first

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
+import { useState } from "react";
+import Intro from "./components/Intro";
 import Hero from "./components/Hero";
 import Lore from "./components/Lore";
 import Trailer from "./components/Trailer";
 import Watch from "./components/Watch";
-
 import Footer from "./components/Footer";
 
 function App() {
+  const [showIntro, setShowIntro] = useState(true);
+
   return (
     <>
-      <Hero />
-      
-      <Lore />
-      <Trailer />
-      <Watch />
-      
-      <Footer />
+      {showIntro ? (
+        <Intro onFinish={() => setShowIntro(false)} />
+      ) : (
+        <>
+          <Hero />
+          <Lore />
+          <Trailer />
+          <Watch />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
